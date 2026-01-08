ca6786df3dce91994a3ca7f58828d8526aa61401
Rolando Bazoalto <rbazoalto@engage2excel.com>
2026-01-08 17:24:03 -0400

    First version of the calculator. 1. Support a maximum of 2 numbers using a comma delimiter. Throw an exception when more than 2 numbers are provided - examples: 20 will return 20; 1,5000 will return 5001; 4,-3 will return 1 - empty input or missing numbers should be converted to 0 - invalid numbers should be converted to 0 e.g. 5,tytyt will return 5

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Add(string input)
+        {
+            string[] numberInput = input.Split(',');
+
+            if (numberInput.Length > 2)
+            {
+                throw new ArgumentException();
+            }
+
+            int value = 0;
+            int response = 0;
+            bool success = false;
+
+            foreach (string item in numberInput)
+            {
+                item.Trim();
+                success = int.TryParse(item, out value);
+
+                if (success)
+                {
+                    response += value;
+                }
+            }
+
+            return response;
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,6 +7,24 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Calculator myCalc = new Calculator();
+            Console.WriteLine("Please enter the number you would like to add (i.e. '1,3')");
+            string input = Console.ReadLine();
+
+            try
+            {
+                int res = myCalc.Add(input);
+                Console.WriteLine("{0} = {1}.", input, res);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("{0} is not correct!", input);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
