97d6adb0deeca62279f10b74f1ec713ec3248f87
Rolando Bazoalto <rbazoalto@engage2excel.com>
2026-01-09 13:49:52 -0400

    7. Support 1 custom delimiter of any length using the format: //[{delimiter}]\n{numbers} - example: //[***]\n11***22***33 will return 66 - all previous formats should also be supported

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -14,31 +14,33 @@ namespace Calculator
         /// <returns>It returns the sum of the numbers.</returns>
         public int Add(string input)
         {
-            char delimiter = '\0';
-            int delimiterLength = GetCustomDelimiter(input, out delimiter);
+            string delimiterString = string.Empty;
+            bool hasLongDelimiter = input.StartsWith("//[") && input.Contains("]");
+            int delimiterLength = GetCustomDelimiter(input, hasLongDelimiter, out delimiterString);
 
             if (delimiterLength > 0)
             {
+                // We remove the delimiter definition including //\n chars
                 input = input.Substring(delimiterLength + 4);
             }
-            char[] delimiters = new char[] { ',', '\n', delimiter};
-            // this is to replace the character \n in the input for the real end of line character. 
-            input = input.Replace("\\n", "\n");
-            string[] numberInput = input.Split(delimiters);
+            string[] delimiters = new string[] { ",", "\\n", delimiterString};
+
+            string[] numberInput = input.Split(delimiters, StringSplitOptions.None);
 
             int value = 0;
             int response = 0;
             bool success = false;
             string negativeNumbers = string.Empty;
             int length = 0;
+            int upperBound = 1000;
 
             foreach (string item in numberInput)
             {
                 item.Trim();
                 success = int.TryParse(item, out value);
 
-                // Only if this is a valid number smaller than 1001, we will add it. 
-                if (success && value < 1001)
+                // Only if this is a valid number smaller and equal than the upper bound, we will add it. 
+                if (success && value <= upperBound)
                 {
                     if (value < 0)
                     {
@@ -60,6 +62,30 @@ namespace Calculator
         }
 
         /// <summary>
+        /// We parse the input to find the delimiter
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="hasLongDelimiter">A flag to handle long delimiters</param>
+        /// <param name="delimiterString"></param>
+        /// <returns>The size of the delimiter</returns>
+        private int GetCustomDelimiter(string input, bool hasLongDelimiter, out string delimiterString)
+        {
+            delimiterString = string.Empty;
+            char delimiter = '\0';
+            int delimiterLength = -1;
+            if (!hasLongDelimiter)
+            {
+                delimiterLength = GetCustomDelimiter(input, out delimiter);
+                delimiterString = delimiter.ToString();
+                return delimiterLength;
+            }
+
+            delimiterLength = GetCustomDelimiter(input, out delimiterString);
+            // We need to fix the length by adding 2 because of the brackets []
+            return delimiterLength + 2;
+        }
+
+        /// <summary>
         /// We parse the input to find the delimiter char
         /// </summary>
         /// <param name="input"></param>
@@ -73,6 +99,25 @@ namespace Calculator
             {
                 delimiter = input[2];
                 return 1;
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// We parse the input to find the delimiter string
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="delimiter"></param>
+        /// <returns>The size of the delimiter. -1 if no custom delimiter was found.</returns>
+        private int GetCustomDelimiter(string input, out string delimiter)
+        {
+            delimiter = string.Empty;
+            int endOfDelimiterIndex = input.IndexOf("]\\n");
+            if (input.StartsWith("//[") && input.Length > endOfDelimiterIndex + 2)
+            {
+                delimiter = input.Substring(3, endOfDelimiterIndex - 3);
+                return delimiter.Length;
             }
 
             return -1;
