218c240d2fb464d17041324203d7788af04e77aa
Rolando Bazoalto <rbazoalto@engage2excel.com>
2026-01-08 22:43:27 -0400

    3. Support a newline character as an alternative delimiter e.g. 1\n2,3 will return 6

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -6,9 +6,17 @@ namespace Calculator
 {
     public class Calculator
     {
+        /// <summary>
+        /// This method adds 2 or more numbers.
+        /// the niput is a string that contains the numbers separated by commas or \n
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>It returns the sum of the numbers.</returns>
         public int Add(string input)
         {
-            string[] numberInput = input.Split(',');
+            // this is to replace the character \n in the input for the real end of line character. 
+            input = input.Replace("\\n", "\n");
+            string[] numberInput = input.Split(new[] { ',', '\n' });
 
             int value = 0;
             int response = 0;
@@ -19,6 +27,7 @@ namespace Calculator
                 item.Trim();
                 success = int.TryParse(item, out value);
 
+                // Only if this is a valid number, we will add it. 
                 if (success)
                 {
                     response += value;
