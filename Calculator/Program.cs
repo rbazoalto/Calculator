d553ad2d9bf0a0465a9980e043159e0e6e99994a
Rolando Bazoalto <rbazoalto@engage2excel.com>
2026-01-09 15:29:06 -0400

    2. Allow the application to process entered entries until Ctrl+C is used

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,22 +9,30 @@ namespace Calculator
             Console.WriteLine("Hello World!");
 
             Calculator myCalc = new Calculator();
-            Console.WriteLine("Please enter the number you would like to add (i.e. '1,3')");
-            string input = Console.ReadLine();
+            string input = string.Empty;
             string operation = string.Empty;
 
-            try
+            // We will iterate until Ctrl-C is pressed.
+            while (true)
             {
-                int res = myCalc.Add(input, out operation);
-                Console.WriteLine("{0} = {1}.", operation, res);
-            }
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine("Negative numbers are not allowed: {0}.", ex.Message);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine("\nPlease enter the number you would like to add (i.e. '1,3')");
+                Console.WriteLine("You can add your own separators (i.e. '//[*][ff]\\n1*3')");
+                Console.Write("Your input: ");
+                input = Console.ReadLine();
+
+                try
+                {
+                    int res = myCalc.Add(input, out operation);
+                    Console.WriteLine("{0} = {1}.", operation, res);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine("Negative numbers are not allowed: {0}.", ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
         }
     }
